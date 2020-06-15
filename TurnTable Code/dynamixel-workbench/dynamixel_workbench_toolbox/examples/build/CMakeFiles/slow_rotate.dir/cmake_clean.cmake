a9d799c28a750af8b67a7c46f1dbde3191573134
Christafuz <christopher.toner87@gmail.com>
2020-06-15 15:39:13 -0700

    Added subfolder

---
+++ TurnTable Code/dynamixel-workbench/dynamixel_workbench_toolbox/examples/build/CMakeFiles/slow_rotate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/slow_rotate.dir/src/slow_rotate.cpp.o"
+  "slow_rotate.pdb"
+  "slow_rotate"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/slow_rotate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
